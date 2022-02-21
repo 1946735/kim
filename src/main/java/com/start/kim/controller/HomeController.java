c8c0394208ca1969dce5a9ab02f3a2c25e242958
1946735 <1946735@naver.com>
2022-02-21 17:02:11 +0900

    3rd update : html href tag 새창띄우기 controller : "/kimins" 추가 깃헙, 블로그 : target tag추가

--- src/main/java/com/start/kim/controller/HomeController.java
+++ src/main/java/com/start/kim/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("/kimins")
     public String home(){
         return "index";
     }
